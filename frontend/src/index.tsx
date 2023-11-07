13666be98224bdfb52e6491adb9a48540043147e
Serhii Nikolin <sergeynikolin@gmail.com>
2023-11-07 20:14:34 +0200

    test3

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import Stet from './Stet/Stet';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Stet />
-  </BrowserRouter>,
+  </HashRouter>,
 );
