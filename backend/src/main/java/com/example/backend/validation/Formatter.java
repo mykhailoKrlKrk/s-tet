9a07ec9111bbc43b6f6af86d725895f1ff208002
Mykhailo Kuryk <113907200+mishaa16@users.noreply.github.com>
2023-11-14 16:06:07 +0200

    add validation fixes for comments

---
+++ backend/src/main/java/com/example/backend/validation/Formatter.java
@@ -0,0 +1,19 @@
+package com.example.backend.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = StringValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Formatter {
+    String message() default "Invalid comment format";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ backend/src/main/java/com/example/backend/validation/StringValidator.java
@@ -0,0 +1,15 @@
+package com.example.backend.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.regex.Pattern;
+
+public class StringValidator implements ConstraintValidator<Formatter, String> {
+    public static final String COMMENT_PATTERN =
+            "^(?![\\p{P}\\s]+$)(?!\\d+$)(?!(.)\\1*$)(?!.*[!&@*#]+)(?!.*\\d).*";
+
+    @Override
+    public boolean isValid(String comment, ConstraintValidatorContext constraintValidatorContext) {
+        return comment != null && Pattern.compile(COMMENT_PATTERN).matcher(comment).matches();
+    }
+}
